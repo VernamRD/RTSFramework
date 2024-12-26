cf3eb32628ee1f04520ffb3aba94b28ae852bb7f
VernamRD <vernamrdcross@mail.ru>
2024-12-27 01:55:19 +0300

    init

---
+++ Source/GLT/Private/GLT.cpp
@@ -0,0 +1,17 @@
+﻿#include "GLT.h"
+
+#define LOCTEXT_NAMESPACE "FGLTModule"
+
+void FGLTModule::StartupModule()
+{
+    
+}
+
+void FGLTModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FGLTModule, GLT)
